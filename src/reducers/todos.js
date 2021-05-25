dd6e84499bd78b4aa8b59cdba4db585cfd9ad0f8
Bojana <bojanabojic30@gmail.com>
2021-05-25 22:24:51 +0200

    app reorganized

---
+++ src/reducers/todos.js
@@ -0,0 +1,10 @@
+const initialState = {
+  name: "Dont be sad Bojana!",
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
